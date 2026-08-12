dee1255bab6ec9deac50e6d73bcadc36b90060e1
Shivtej126543 <sasukeisnerfed1@gmail.com>
2026-08-12 13:29:36 +0530

    Create marksheet.sql

---
+++ marksheet.sql
@@ -0,0 +1,12 @@
+create table Marksheet (
+  ROLL int primary key,
+  NAME varchar(20),
+  DSA int,
+  ACD int,
+  DBMS int
+);
+insert into Marksheet values (1, 'Shivtej', 20, 19, 25),
+ (2, 'Surmayee', 21, 18, 30),
+ (3, 'Jalpan', 20, 15, 24),
+ (4, 'Ranveersingh', 21, 17, 30),
+(5, 'Soham', 17, 20, 29);
