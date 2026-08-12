55be1fdedb1aed2c75f99c91b820b6a145dfe679
Shivtej126543 <sasukeisnerfed1@gmail.com>
2026-08-12 12:08:17 +0530

    Create student.sql

---
+++ student.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Communique(
+    Student_id int,
+    name varchar(20),
+    Designation varchar(20)
+);
+INSERT INTO Student (Roll_Number, name, branch)
+
+VALUES(1, 'Surmayee Deshpande', 'Executive Member'),
+(2, 'Shivtej Ambilduke', 'Executive Member');
